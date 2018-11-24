a300977e5d1f686e7fdfcd278c7c134c9154a324
Wil Cook <wil.cook.is@gmail.com>
2018-11-24 15:32:17 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -8,7 +8,7 @@ def my_select(collection)
   collection[i]
   yield(collection[i])
   if yield(collection[i]) == true
-    new_array << yield(collection[i])
+    new_array << collection[i]
   end
   i += 1 
  end
