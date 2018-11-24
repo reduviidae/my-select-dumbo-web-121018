5060b5e78210691bcbc4ad074084999ba5b86db4
Wil Cook <wil.cook.is@gmail.com>
2018-11-24 15:33:08 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
+# require 'pry'
+
 def my_select(collection)
- # your code here!
+ i = 0 
+ new_array = []
+ while i < collection.length
+ # binding.pry
+  collection[i]
+  yield(collection[i])
+  if yield(collection[i]) == true
+    new_array << collection[i]
+  end
+  i += 1 
+ end
+ new_array
 end
