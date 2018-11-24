471108ae09d3dec6eecb22fd22925e8b863da9d0
Wil Cook <wil.cook.is@gmail.com>
2018-11-24 15:17:06 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,10 +3,10 @@ require 'pry'
 def my_select(collection)
  i = 0 
  new_array = []
- while i < array.length
+ while i < collection.length
   binding.pry
-  yield(array[i])
-  new_array << yield(array[i])
+  yield(collection[i])
+  new_array << yield(collection[i])
   i += 1 
  end
 end
